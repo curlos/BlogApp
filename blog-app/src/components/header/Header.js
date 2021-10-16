169544d84f9a8a893707b8cad299a8a0e8d63f5e
Carlos Martinez <curlosmart@gmail.com>
2021-10-16 09:35:23 -0400

    Header

---
+++ blog-app/src/components/header/Header.js
@@ -0,0 +1,46 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import './Header.css'
+
+const Header = () => {
+
+  return (
+    <div>
+      <div className="headerContainerTop">
+        <div className="searchButton">
+          <i className="fas fa-search"></i>
+        </div>
+
+        <div className="mainTitle">
+          <Link to="/">The Blog</Link>
+        </div>
+
+        <div className="userButtons">
+          <button>
+            <Link to="/new-post">Write</Link>
+          </button>
+          <button>Login</button>
+          <button>Register</button>
+        </div>
+      </div>
+
+      <div className="headerContainerBottom">
+        <span>TECH</span>
+        <span>LIFE</span>
+        <span>SPORTS</span>
+        <span>ART</span>
+        <span>FOOD</span>
+        <span>DIY</span>
+        <span>HEALTH</span>
+        <span>FITNESS</span>
+      </div>
+    </div>
+  )
+}
+
+export default Header;
