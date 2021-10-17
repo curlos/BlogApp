818fbbaf8540fa4a1f2c0e585f3c6c5254463b99
Carlos Martinez <curlosmart@gmail.com>
2021-10-17 09:21:48 -0400

    Database and server setup

---
+++ server/server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const session = require("express-session");
+const dotenv = require('dotenv').config()
+const passport = require("passport");
+const cors = require('cors')
+const PORT = process.env.PORT || 8888
+const postsRouter = require('./routers/posts')
+const usersRouter = require('./routers/users')
+const commentsRouter = require('./routers/comments')
+const database = require('./database/connection')
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+
+app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+require('./passport/config')(passport)
+
+app.use('/posts', postsRouter)
+app.use('/users', usersRouter)
+app.use('/comments', commentsRouter)
+
+app.listen(PORT, () => {
+  database.connectToServer((err) => {
+    if (err) {
+      console.error(err)
+    }
+  })
+
+  console.log(`Server is listening on port ${PORT}`)
+})
