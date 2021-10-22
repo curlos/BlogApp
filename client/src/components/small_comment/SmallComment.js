a8f955f3d49cae6455f8e2f211b7f60ff172a5f4
Carlos Martinez <curlosmart@gmail.com>
2021-10-22 10:00:30 -0400

    handle deleted posts for small comments

--- client/src/components/small_comment/SmallComment.js
+++ client/src/components/small_comment/SmallComment.js
@@ -3,6 +3,25 @@ import moment from 'moment'
 import './SmallComment.css'
 
 const SmallComment = ({ comment, post }) => {
+
+  if(!post) {
+    return (
+      <div className="smallCommentContainer">
+        <div className="smallCommentHeader">
+          <span>[DELETED]</span>
+          <span className="smallCommentFromNow">{moment(comment.createdAt).fromNow()}</span>
+        </div>
+
+        <div>
+          {comment.content}
+        </div>
+
+        <div>
+          <span><i class="fas fa-thumbs-up"></i> {comment.likes.length - comment.dislikes.length}</span>
+        </div>
+      </div>
+    )
+  }
   
   return (
     <Link to={`/post/${post._id}`}>
@@ -17,7 +36,7 @@ const SmallComment = ({ comment, post }) => {
         </div>
 
         <div>
-          <span><i class="far fa-heart"></i> {comment.likes.length}</span>
+          <span><i class="fas fa-thumbs-up"></i> {comment.likes.length - comment.dislikes.length}</span>
         </div>
       </div>
     </Link>
