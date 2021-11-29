41bc2b576d97efc1ed60ecf24627de1f52aa035f
Carlos Martinez <curlosmart@gmail.com>
2021-11-28 22:33:51 -0500

    cors test

--- server/server.js
+++ server/server.js
@@ -15,8 +15,9 @@ const path = require("path")
 
 const app = express()
 
-app.use(logger('dev'))
 app.use(cors())
+
+app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 
